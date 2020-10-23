f8d3065e93570ef47dddb3a931901d048fbe1458
中村陸人 <>
2020-10-24 05:53:30 +0900

    動画の固定、解除機能の追加

--- content.js
+++ content.js
@@ -1,9 +1,15 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendRespose) {
-  if (message === 'scroll') {
-    
-  }
   if (message === 'movie') {
     var elements = document.getElementsByClassName('html5-video-container');
     elements[0].style.position = 'fixed';
+  }
+
+  if (message === 'movieRelease') {
+    var elements = document.getElementsByClassName('html5-video-container');
+    elements[0].style.position = 'static';
+  }
+  
+  if (message === 'scroll') {
+    
   }
 })
--- popup.js
+++ popup.js
@@ -3,10 +3,24 @@ const pushMessageToContent = function (message) {
     const tab = tabs[0]
     chrome.tabs.sendMessage(tab.id, message)
   })
- }
- document.getElementById('scroll_button').addEventListener('click', function() {
+}
+
+document.getElementById('movie_button').addEventListener('click', function() {
+  pushMessageToContent('movie');
+  const movie = document.getElementById('movie_button');
+  const movieRelease = document.getElementById('movie_release');
+  movie.classList.add('hidden');
+  movieRelease.classList.remove('hidden');
+})
+
+document.getElementById('movie_release').addEventListener('click', function() {
+  pushMessageToContent('movieRelease');
+  const movie = document.getElementById('movie_button');
+  const movieRelease = document.getElementById('movie_release');
+  movie.classList.remove('hidden');
+  movieRelease.classList.add('hidden');
+})
+
+document.getElementById('scroll_button').addEventListener('click', function() {
   pushMessageToContent('scroll') 
- })
- document.getElementById('movie_button').addEventListener('click', function() {
-  pushMessageToContent('movie') 
- })
+})
